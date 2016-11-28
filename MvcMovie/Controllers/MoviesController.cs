e75a274c521a7a8278c0c2103a7ed6b17f088574
David Reyes Pucheta <davidpucheta@gmail.com>
2016-11-28 17:23:12 -0600

    Add Database

---
+++ MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+	{
+
+		private MovieDBContext db = new MovieDBContext();
+
+		//Get: /Movies/
+        public ActionResult Index()
+        {
+			return View (db.Movies.ToList());
+        }
+
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+			}
+
+			Movie movie = db.Movies.Find(id);
+
+			if (movie == null)
+			{
+				return HttpNotFound();
+			}
+
+			return View(movie);
+		}
+
+    }
+}
